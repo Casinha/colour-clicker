7b8cbe384d0ddff7ada794d78569e5928377202c
Casinha <thehighredeemer@hotmail.com>
2023-08-23 05:23:52 +0100

    Debugging GITHUB_ACTIONS trigger for config

--- next.config.js
+++ next.config.js
@@ -2,12 +2,14 @@
 const nextConfig = {
 }
 
-if (process.env.GITHUB_ACTIONS) {
+if (process.env.GITHUB_ACTIONS || false) {
     const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
     
     nextConfig.assetPrefix = `/${repo}/`
     nextConfig.basePath = `/${repo}`
     nextConfig.output = "export"
 }
+
+console.log(`Next Config`, nextConfig)
 
 module.exports = nextConfig
