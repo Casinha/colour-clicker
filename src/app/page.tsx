ba6a90e8cbfc5d33ca090e97a8f8b82fc99b40de
Casinha <thehighredeemer@hotmail.com>
2023-08-23 04:00:45 +0000

    Deploying to gh-pages from @ Casinha/colour-clicker@84d22789b118d84b20f0c4c0e2eac21d719fdd2c 🚀

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Game from './components/game/game.component'
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Game></Game>
-    </main>
-  )
-}
